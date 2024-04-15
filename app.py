11a8cfc3111225650bab25060d8971df21962b73
Rick <i461577@student.fontys.nl>
2024-04-15 16:42:48 +0200

    worked on a new frontend, rather started from scratch then fixed the working one

--- app.py
+++ app.py
@@ -1,18 +1,38 @@
+from concurrent.futures import thread
 import sys
 import threading
 import json
-from flask import Flask, render_template, jsonify, request, make_response
+import os
+from pathlib import Path
+from flask import Flask, render_template, jsonify, request, make_response, abort, send_file
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 import pyqtgraph as pg
 
-app = Flask(__name__)
+app = Flask(__name__,template_folder='new_frontend/')
 position_data = {}; 
 
-@app.route("/")
-def home():
-    return render_template('index.html')
+@app.route("/", endpoint='template')
+@app.route("/<page>/")
+def home(page=None):
+    sidebar = None
+    extra_style = None
+    header_content = None
+    if(page is None): #set default to current-game
+        page = "current-game"
+    if(not os.path.exists("./new_frontend/"+page)):
+        return  abort(404)
+    if(os.path.exists("./new_frontend/"+page+"/sidebar.html")):
+        sidebar=page+"/sidebar.html"
+    if(os.path.exists("./new_frontend/"+page+"/style.css")):
+        extra_style=page+"/style.css"
+    if(os.path.exists("./new_frontend/"+page+"/header.html")):
+        header_content="./new_frontend/"+page+"/header.html"
+    return render_template('/index.html', page=page, sidebar=sidebar, header_content=header_content, extra_style=extra_style)
+@app.route("/<file>", endpoint='resources')
+def get_resources(file):
+    return send_file(file)
 
 @app.route("/robot_positions")
 def robot_positions():
@@ -43,22 +63,20 @@ def import_data_file():
     except Exception as e:
         # log the exception (optional)
         print(f"An error occurred: {e}", file=sys.stderr)
-        
-
+    print("done importing")
 def run_flask():
     app.run(debug=False, port=5000)
 
 if __name__ == '__main__':
-    threading.Thread(target=run_flask).start() # create new thread allowing to run flask in background  without blocking execution of other code
-
+    threading.Thread(target=import_data_file).start()
+    threading.Thread(target=app.run(debug=False, port=5000)).start() # create new thread allowing to run flask in background  without blocking execution of other code
     # PyQt application 
-    import_data_file()
-    app = QApplication(sys.argv) # initialize app with allow command-line
+    QTapp = QApplication(sys.argv) # initialize app with allow command-line
     # mainWindow = QMainWindow() # creates main window
     webEngineView = QWebEngineView() # creates widget to display web pages
     webEngineView.load(QUrl("http://localhost:5000"))
     # mainWindow.setCentralWidget(webEngineView) 
     # mainWindow.show() # displays main window 
-    sys.exit(app.exec_()) # start app waiting for user interaction with ensures app will exit when terminated
+    sys.exit(QTapp.exec_()) # start app waiting for user interaction with ensures app will exit when terminated
 
 
