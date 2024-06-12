e30fc639637cb63af4848c1b53ab5f6dd9ce9bd4
Rick <i461577@student.fontys.nl>
2024-06-12 11:32:31 +0200

    unfinished new message type bytearr.

--- testclient.py
+++ testclient.py
@@ -7,9 +7,10 @@ sobj = socket(AF_INET, SOCK_DGRAM)
 ip = "127.0.0.1"
 port = 5050
 msg = str("{\"id\":\"0\",\"pos\":{ \"x\":0,\"y\":0,\"rz\":0}}")
+bytemsg = int.from_bytes(b'0x00',"big")
 bytesSend = None
 while True:
-    bytesSend = sobj.sendto(msg.encode(), (ip, port))
+    bytesSend = sobj.sendto(b'\x00\x00\x00\x00\xff\xff\xff\xff', (ip, port))
     if(bytesSend < 0):
         print("couldn't send bytes, did is the server running")
         exit()
